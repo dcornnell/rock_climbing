ae6638dc781ae80d740a667cfeb0f996d5b251d7
Daniel Cornnell <daniel.cornnell@gmail.com>
2014-05-04 16:37:14 -0400

    made score filter by dates

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -3,6 +3,10 @@ class Attempt < ActiveRecord::Base
 	belongs_to :route
 	validates :date_attempted, presence: true
 
+	scope :this_day, -> {where('date_attempted > ?' , Date.today)}
+	scope :this_week, -> {where('date_attempted > ?', 7.days.ago)}
+	scope :this_month, -> {where('date_attempted > ? ',   30.days.ago)}
+	scope :this_year, -> {where('date_attempted > ? ',   365.days.ago)}
 
 	def add_modifiers(grade, completed, flash)
 
@@ -19,6 +23,10 @@ class Attempt < ActiveRecord::Base
 			grade = (grade * 10).round
 		end
 end
+
+
+
+
 
 
 		
